5a889d05fd675f8ff9b2ce6dc8b9c55874c740f3
Admin Apple <adminapple@ackerlaptop.local>
2018-05-08 13:16:47 -0400

    fix

--- AgmOverlay.component.d.ts
+++ AgmOverlay.component.d.ts
@@ -1,18 +1,21 @@
 import { ElementRef } from "@angular/core";
 import { MarkerManager, GoogleMapsAPIWrapper } from "@agm/core";
-import { GoogleMap } from "@agm/core/services/google-maps-types";
 export declare class AgmOverlay {
     protected _mapsWrapper: GoogleMapsAPIWrapper;
     private _markerManager;
     overlayView: any;
+    elmGuts: any;
     latitude: number;
     longitude: number;
     visible: boolean;
     template: ElementRef;
     constructor(_mapsWrapper: GoogleMapsAPIWrapper, _markerManager: MarkerManager);
+    ngAfterViewInit(): void;
     ngOnChanges(changes: any): void;
+    onChanges(changes: any): void;
+    onChangesOverride(changes: any): void;
     ngOnDestroy(): void;
     destroy(): void;
-    load(): void;
-    drawOnMap(map: GoogleMap): void;
+    load(): Promise<void>;
+    getOverlay(map: any): any;
 }
--- AgmOverlay.component.js
+++ AgmOverlay.component.js
@@ -8,62 +8,88 @@ var AgmOverlay = (function () {
         this._markerManager = _markerManager;
         this.visible = true;
     }
+    AgmOverlay.prototype.ngAfterViewInit = function () {
+        var _this = this;
+        this.elmGuts = this.template.nativeElement.children[0];
+        this.load().then(function () {
+            _this.onChanges = _this.onChangesOverride;
+        });
+    };
     AgmOverlay.prototype.ngOnChanges = function (changes) {
+        this.onChanges(changes);
+    };
+    AgmOverlay.prototype.onChanges = function (changes) { };
+    AgmOverlay.prototype.onChangesOverride = function (changes) {
+        var _this = this;
         if ((changes.latitude || changes.longitude)) {
-            if (this.overlayView && this.overlayView.draw) {
-                this.overlayView.draw();
-            }
-            else {
-                this.load();
-            }
+            this.overlayView.latitude = this.latitude;
+            this.overlayView.longitude = this.longitude;
+            this._markerManager.deleteMarker(this.overlayView)
+                .then(function () { return _this.load(); });
         }
     };
     AgmOverlay.prototype.ngOnDestroy = function () {
         this.destroy();
     };
     AgmOverlay.prototype.destroy = function () {
+        this._markerManager.deleteMarker(this.overlayView);
         this.overlayView.setMap(null);
         delete this.overlayView;
+        delete this.elmGuts;
     };
     AgmOverlay.prototype.load = function () {
         var _this = this;
-        this._mapsWrapper.getNativeMap()
+        return this._mapsWrapper.getNativeMap()
             .then(function (map) {
-            _this.drawOnMap(map);
-            _this._markerManager.addMarker(_this.overlayView);
-            return _this._markerManager.getNativeMarker(_this.overlayView);
+            var overlay = _this.getOverlay(map);
+            _this._markerManager.addMarker(overlay);
+            return _this._markerManager.getNativeMarker(overlay);
         })
             .then(function (nativeMarker) {
             var setMap = nativeMarker.setMap;
+            if (nativeMarker['map']) {
+                _this.overlayView.setMap(nativeMarker['map']);
+            }
             nativeMarker.setMap = function (map) {
                 setMap.call(nativeMarker, map);
-                _this.overlayView.setMap(map);
+                if (_this.overlayView) {
+                    _this.overlayView.setMap(map);
+                }
             };
         });
     };
-    AgmOverlay.prototype.drawOnMap = function (map) {
+    AgmOverlay.prototype.getOverlay = function (map) {
         this.overlayView = this.overlayView || new google.maps.OverlayView();
         this.overlayView.iconUrl = " ";
         this.overlayView.latitude = this.latitude;
         this.overlayView.longitude = this.longitude;
-        var latlng = new google.maps.LatLng(this.latitude, this.longitude);
-        var elm = this.template.nativeElement.children[0];
+        var elm = this.elmGuts;
         this.overlayView.remove = function () {
             this.div.parentNode.removeChild(this.div);
             delete this.div;
         };
+        this.overlayView.getDiv = function () {
+            return this.div;
+        };
         this.overlayView.draw = function () {
             if (!this.div) {
                 this.div = elm;
-                this.getPanes().overlayImage.appendChild(elm);
+                var panes = this.getPanes();
+                if (!panes || !panes.overlayImage)
+                    return;
+                panes.overlayImage.appendChild(elm);
             }
-            var point = this.getProjection().fromLatLngToDivPixel(latlng);
+            var latlng = new google.maps.LatLng(this.latitude, this.longitude);
+            var proj = this.getProjection();
+            if (!proj)
+                return;
+            var point = proj.fromLatLngToDivPixel(latlng);
             if (point) {
                 elm.style.left = (point.x - 10) + 'px';
                 elm.style.top = (point.y - 20) + 'px';
             }
         };
-        this.overlayView.setMap(map);
+        return this.overlayView;
     };
     AgmOverlay.decorators = [
         { type: core_1.Component, args: [{

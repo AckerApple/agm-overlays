e665c3e13fe5ddb17dbf70dd4b6ed88427b54dc6
Admin Apple <adminapple@ackerlaptop.local>
2018-05-16 10:55:34 -0400

    x

--- AgmOverlay.component.js
+++ AgmOverlay.component.js
@@ -6,14 +6,28 @@ var AgmOverlay = (function () {
     function AgmOverlay(_mapsWrapper, _markerManager) {
         this._mapsWrapper = _mapsWrapper;
         this._markerManager = _markerManager;
+        this._observableSubscriptions = [];
         this.visible = true;
+        this.zIndex = 1;
+        this.markerClick = new core_1.EventEmitter();
+        this.openInfoWindow = true;
+        this.infoWindow = new core_1.QueryList();
+        this.draggable = false;
     }
     AgmOverlay.prototype.ngAfterViewInit = function () {
         var _this = this;
         this.elmGuts = this.template.nativeElement.children[0];
+        var iWins = this.template.nativeElement.getElementsByTagName('agm-info-window');
+        for (var x = iWins.length - 1; x >= 0; --x) {
+            iWins[x].parentNode.removeChild(iWins[x]);
+        }
         this.load().then(function () {
             _this.onChanges = _this.onChangesOverride;
         });
+    };
+    AgmOverlay.prototype.ngAfterContentInit = function () {
+        var _this = this;
+        this.infoWindow.changes.subscribe(function () { return _this.handleInfoWindowUpdate(); });
     };
     AgmOverlay.prototype.ngOnChanges = function (changes) {
         this.onChanges(changes);
@@ -34,8 +48,18 @@ var AgmOverlay = (function () {
     AgmOverlay.prototype.destroy = function () {
         this._markerManager.deleteMarker(this.overlayView);
         this.overlayView.setMap(null);
+        this._observableSubscriptions.forEach(function (s) { return s.unsubscribe(); });
         delete this.overlayView;
         delete this.elmGuts;
+    };
+    AgmOverlay.prototype.handleInfoWindowUpdate = function () {
+        var _this = this;
+        if (this.infoWindow.length > 1) {
+            throw new Error('Expected no more than one info window.');
+        }
+        this.infoWindow.forEach(function (iWin) {
+            iWin.hostMarker = _this.overlayView;
+        });
     };
     AgmOverlay.prototype.load = function () {
         var _this = this;
@@ -43,6 +67,7 @@ var AgmOverlay = (function () {
             .then(function (map) {
             var overlay = _this.getOverlay(map);
             _this._markerManager.addMarker(overlay);
+            _this._addEventListeners();
             return _this._markerManager.getNativeMarker(overlay);
         })
             .then(function (nativeMarker) {
@@ -59,12 +84,15 @@ var AgmOverlay = (function () {
         });
     };
     AgmOverlay.prototype.getOverlay = function (map) {
+        var _this = this;
         this.overlayView = this.overlayView || new google.maps.OverlayView();
         this.overlayView.iconUrl = " ";
         this.overlayView.latitude = this.latitude;
         this.overlayView.longitude = this.longitude;
         var elm = this.elmGuts;
         this.overlayView.remove = function () {
+            if (!this.div)
+                return;
             this.div.parentNode.removeChild(this.div);
             delete this.div;
         };
@@ -89,7 +117,23 @@ var AgmOverlay = (function () {
                 elm.style.top = (point.y - 20) + 'px';
             }
         };
+        elm.addEventListener("click", function (event) { return _this.handleTap(); });
+        this.handleInfoWindowUpdate();
         return this.overlayView;
+    };
+    AgmOverlay.prototype.handleTap = function () {
+        if (this.openInfoWindow) {
+            this.infoWindow.forEach(function (infoWindow) {
+                infoWindow.open();
+            });
+        }
+        this.markerClick.emit(null);
+    };
+    AgmOverlay.prototype._addEventListeners = function () {
+        var _this = this;
+        var eo = this._markerManager.createEventObservable('click', this.overlayView);
+        var cs = eo.subscribe(function () { return _this.handleTap(); });
+        this._observableSubscriptions.push(cs);
     };
     AgmOverlay.decorators = [
         { type: core_1.Component, args: [{
@@ -105,6 +149,11 @@ var AgmOverlay = (function () {
         "latitude": [{ type: core_1.Input },],
         "longitude": [{ type: core_1.Input },],
         "visible": [{ type: core_1.Input },],
+        "zIndex": [{ type: core_1.Input },],
+        "markerClick": [{ type: core_1.Output },],
+        "openInfoWindow": [{ type: core_1.Input },],
+        "infoWindow": [{ type: core_1.ContentChildren, args: [core_2.AgmInfoWindow,] },],
+        "draggable": [{ type: core_1.Input, args: ['markerDraggable',] },],
         "template": [{ type: core_1.ViewChild, args: ['content', { read: core_1.ElementRef },] },],
     };
     return AgmOverlay;

6e14e0dbf9f35a2c4e1df4af84bc0e400a0a51dc
Acker Apple <ackerapple@Ackers-Mac-mini.local>
2020-02-09 10:31:33 -0500

    x

--- scripts/update-dist-package.js
+++ scripts/update-dist-package.js
@@ -6,7 +6,7 @@ const pack = require(packPath)
 
 pack.version = rootPack.version
 pack.jsDependencies = rootPack.jsDependencies
-
+delete pack.private;
 fs.writeFileSync(packPath, JSON.stringify(pack, null, 2))
 const path = require('path')
 
@@ -14,12 +14,11 @@ const fromReadMe = path.join(packPath,'../../','README.md')
 const toReadMe = path.join(packPath,'../','README.md')
 fs.writeFileSync(toReadMe, fs.readFileSync(fromReadMe))
 
-console.log('\x1b[33m[ack-angular]:\x1b[0m', 'updated dist package version to ', pack.version)
-
 function manageExample(){
   const exPackPath = require.resolve('../example/package.json')
   const exPack = require(exPackPath)
   exPack.devDependencies = rootPack.devDependencies
+  delete exPack.private;
   fs.writeFileSync(exPackPath, JSON.stringify(exPack, null, 2))
 }
 
